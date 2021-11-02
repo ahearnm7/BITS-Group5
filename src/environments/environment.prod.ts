2576dbb19e728ba4be325d9941c4d27d7272d752
Brian Cusack <89521528+Falcons34@users.noreply.github.com>
2021-11-02 15:51:21 +1100

    API keys added for Firebase and GMaps (#6)
    * Added CSS Gris
    
    * api kes added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAYXmXp7dLKu_h0qbEka2VCPJsJ3Hp7Dao",
+    authDomain: "covisulisers.firebaseapp.com",
+    projectId: "covisulisers",
+    storageBucket: "covisulisers.appspot.com",
+    messagingSenderId: "1096091104099",
+    appId: "1:1096091104099:web:b8703a8edfd6d0e3409956",
+    measurementId: "G-70V5W1BWT7"
+  },
+  gmaps: 'AIzaSyAl_Yl759PbIh9z9eTHgF6_2VlHaYfKKiQ'
 };
