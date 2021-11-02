1e2d9f8623c41914dc22e390d5f705ce39bbd642
Falcons34 <s3407813@student.rmit.edu.au>
2021-11-02 14:42:28 +1100

    Added CSS Gris

--- src/app/views/chart/chartdaily/chartdaily.component.ts
+++ src/app/views/chart/chartdaily/chartdaily.component.ts
@@ -1,12 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartDataSets, ChartOptions, ChartAnimationOptions, ChartType } from 'chart.js';
+import { Label, Colors } from 'ng2-charts';
 @Component({
   selector: 'app-chartdaily',
   templateUrl: './chartdaily.component.html',
   styleUrls: ['./chartdaily.component.scss']
 })
 export class ChartdailyComponent implements OnInit {
+  barChartData: ChartDataSets[] = [{ data: [30, 50, 50, 60, 50, 30, 50, 52, 52], label: 'Line', type: "line" },
+  { data: [0, 65, 59, 80, 81, 56, 55, 40, 0], label: 'Series A' },
+  { data: [0, 28, 48, 40, 19, 86, 27, 90, 0], label: 'Series B' },
 
+  ];
+  barChartLabels: Label[] = ['FirstPlaceholder', '2006', '2007', '2008', '2009', '2010', '2011', '2012', 'LastPlaceholder'];
+  barChartOptions: ChartOptions = {
+    responsive: true,
+    showLines: true,
+
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      xAxes: [{
+        ticks: {
+          min: '2006',
+          max: '2012',
+        }
+      }], yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+
+  };
+  barChartColors: Colors[] = [
+    {
+      borderColor: '#F1BD65',
+      backgroundColor: '#F1BD65',
+    },
+    {
+      borderColor: '#A69DE4',
+      backgroundColor: '#A69DE4',
+    },
+    {
+      borderColor: '#fb99bc',
+      backgroundColor: '#fb99bc',
+    },
+  ];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
   constructor() { }
 
   ngOnInit(): void {
