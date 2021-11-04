45b7bc1bc33d5d59b14dd98edf6a1a20f17f7972
Daniel-Scarfe <S3872217@student.rmit.edu.au>
2021-11-04 23:24:10 +0800

    map setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Covidify';
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyCr-NSSMoEVi725-k6escXY6h8D6LBB1r8'
+    })
+
+    loader.load().then(() => {
+      new google.maps.Map(document.getElementById("map"),{
+        center: { lat: 51.233334, lng: 6.78333},
+        zoom: 6
+      })
+    })
+  }
 }
