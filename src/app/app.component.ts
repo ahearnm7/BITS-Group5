2bc8b783c0b7031bbbbdff87a58fac30b159905f
Daniel-Scarfe <S3872217@student.rmit.edu.au>
2021-11-04 23:42:03 +0800

    maps setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,21 @@ import { Loader } from '@googlemaps/js-api-loader';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Covidify';
 
-  ngOnInit(): void {
-    let loader = new Loader({
-      apiKey: 'AIzaSyCr-NSSMoEVi725-k6escXY6h8D6LBB1r8'
-    })
+//export class AppComponent implements OnInit {
 
-    loader.load().then(() => {
-      new google.maps.Map(document.getElementById("map"),{
-        center: { lat: 51.233334, lng: 6.78333},
-        zoom: 6
-      })
-    })
-  }
+  // ngOnInit(): void {
+  //   let loader = new Loader({
+  //     apiKey: 'AIzaSyCr-NSSMoEVi725-k6escXY6h8D6LBB1r8'
+  //   })
+
+  //   loader.load().then(() => {
+  //     new google.maps.Map(document.getElementById("map"),{
+  //       center: { lat: 51.233334, lng: 6.78333},
+  //       zoom: 6
+  //     })
+  //   })
+  // }
 }
