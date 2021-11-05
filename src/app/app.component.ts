affcbc7d08894dabfae806daa8ff89bb95d70c72
Daniel-Scarfe <S3872217@student.rmit.edu.au>
2021-11-05 10:26:17 +0800

    map setup bug fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +8,4 @@ import { Loader } from '@googlemaps/js-api-loader';
 export class AppComponent {
   title = 'Covidify';
 
-//export class AppComponent implements OnInit {
-
-  // ngOnInit(): void {
-  //   let loader = new Loader({
-  //     apiKey: 'AIzaSyCr-NSSMoEVi725-k6escXY6h8D6LBB1r8'
-  //   })
-
-  //   loader.load().then(() => {
-  //     new google.maps.Map(document.getElementById("map"),{
-  //       center: { lat: 51.233334, lng: 6.78333},
-  //       zoom: 6
-  //     })
-  //   })
-  // }
 }
--- src/app/views/map/gmap/gmap.component.ts
+++ src/app/views/map/gmap/gmap.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-gmap',
@@ -9,7 +10,17 @@ export class GmapComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+     let loader = new Loader({
+       apiKey: 'AIzaSyCr-NSSMoEVi725-k6escXY6h8D6LBB1r8'
+     })
+
+     loader.load().then(() => {
+       new google.maps.Map(document.getElementById("map"),{
+         center: { lat: 51.233334, lng: 6.78333},
+         zoom: 6
+       })
+     })
+   }
 
 }
