ed4a4d10c934fcacf11b12563f01c8047cbda27a
PaulBedward <90188370+PaulBedward@users.noreply.github.com>
2021-11-17 12:39:06 +1100

    Revert "Update to position database age message on map disaply"
    This reverts commit 24ab87ff014d4cf6143fa05cf5b42a49be8b8d10.

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -13,7 +13,7 @@ import { Injectable } from '@angular/core';
 export class MessageService {
     // Just return a number
     getMessage(): number{
-        return 5;
+        return 3;
 
     }
     constructor() { }
