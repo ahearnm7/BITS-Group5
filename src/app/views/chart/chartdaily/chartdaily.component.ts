207f021792125aabd63cbb94dc008fa5aacce162
Brian Cusack <89521528+Falcons34@users.noreply.github.com>
2021-11-18 15:36:01 +1100

    Development (#54)
    * Chart daily (#52)
    
    * Quick fix
    
    * added api key
    
    * Connected chart table FireStore
    
    * set collection
    
    * Update chartdaily.component.ts
    
    quick test
    
    * may break
    
    * breaky
    
    * dashes left over
    
    * login service
    
    * login system - not complete
    
    * style edits on multiple components
    
    * authentication added
    
    * Chart daily (#53)
    
    * Quick fix
    
    * added api key
    
    * Connected chart table FireStore
    
    * set collection
    
    * Update chartdaily.component.ts
    
    quick test
    
    * may break
    
    * breaky
    
    * dashes left over
    
    * login service
    
    * login system - not complete
    
    * style edits on multiple components
    
    * authentication added
    
    * chart edits
    
    * map key added

--- src/app/views/chart/chartdaily/chartdaily.component.ts
+++ src/app/views/chart/chartdaily/chartdaily.component.ts
@@ -12,16 +12,20 @@ export interface Item { name: string; }
 })
 export class ChartdailyComponent implements OnInit {
 
-
-  barChartData: ChartDataSets[] = [];
+  data = this.generatelineNumbers(2, 14);
+  ave = this.generateNumbers(3, 13);
+  barChartData: ChartDataSets[] = [
+    { data: this.data, label: 'Daily Cases', type: 'line' },
+    { data: this.ave, label: 'Ave State Cases' }
+  ];
   chartDataReady = false;
-  barChartLabels: Label[] = ['FirstPlaceholder', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'LastPlaceholder'];
+  barChartLabels: Label[] = ['FirstPlaceholder', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', 'LastPlaceholder'];
   barChartOptions: ChartOptions = {
     responsive: true,
     showLines: true,
     maintainAspectRatio: true,
     title: {
-      text: 'Local Monthly Cases',
+      text: 'Local Daily Cases',
       display: true
     },
 
@@ -30,8 +34,8 @@ export class ChartdailyComponent implements OnInit {
       xAxes: [{
         ticks: {
 
-          min: 'Jan',
-          max: 'Dec',
+          min: '7',
+          max: '18',
 
         }
       }], yAxes: [{
@@ -44,40 +48,146 @@ export class ChartdailyComponent implements OnInit {
   };
   barChartColors: Colors[] = [
     {
-      borderColor: '#F1BD65',
-      backgroundColor: '#F1BD65',
+      borderColor: '#ff9933',
+      backgroundColor: '#ffe6cc',
     },
     {
-      borderColor: '#A69DE4',
-      backgroundColor: '#A69DE4',
+      borderColor: '#999999',
+      backgroundColor: '#999999',
     },
-    {
-      borderColor: '#fb99bc',
-      backgroundColor: '#fb99bc',
-    },
+
   ];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
-  item$: Observable<DocumentData[]>;
+
+  // item$: Observable<DocumentData[]>;
 
 
   constructor(firestore: Firestore) {
-
-    // ---  Set collection doc
-    // const datachart = doc(col);
-    // setDoc(doc(col, 'datachart'), { data: this.barChartData });
-
-    // ---  Get collection doc
-    const colDaily = collection(firestore, 'CaseNumbers/');
-    this.item$ = collectionData(colDaily);
-    this.item$.subscribe((x: DocumentData[]) => {
-      this.barChartData = x[0]?.data;
-      this.chartDataReady = true;
-      console.log(x)
-    });
+    this.chartDataReady = true;
+    this.dailyChart()
   }
 
   ngOnInit(): void {
   }
+  generateNumbers(min: number, max: number) {
+    let arr: number[] = [];
+    for (let index = 0; index < 14; index++) {
+      arr[index] = (Math.floor(min + Math.random() * (max + 3 - 2)))
+    }
+    return (arr)
+  }
+  generatelineNumbers(min: number, max: number) {
+    let arr: number[] = [];
+    for (let index = 0; index < 14; index++) {
+      arr[index] = (Math.floor(min + Math.random() * (max + 3 - 5)))
+    }
+    return (arr)
+  }
+  dailyChart() {
+    this.data = this.generatelineNumbers(4, 14);
+    this.ave = this.generateNumbers(7, 17);
+    this.barChartData = [
+      { data: this.ave, label: 'Ave State Cases', type: 'line' },
+      { data: this.data, label: 'Daily Cases' }
+    ];
+    this.barChartLabels = ['FirstPlaceholder', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', 'LastPlaceholder'];
+    this.barChartOptions = {
+      responsive: true,
+      showLines: true,
+      maintainAspectRatio: true,
+      title: {
+        text: 'Local Daily Cases',
+        display: true
+      },
 
+      // We use these empty structures as placeholders for dynamic theming.
+      scales: {
+        xAxes: [{
+          ticks: {
+
+            min: '7',
+            max: '18',
+
+          }
+        }], yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+
+    };
+  }
+  MonthlyChart() {
+    this.data = this.generatelineNumbers(2, 14);
+    this.ave = this.generateNumbers(5, 30);
+    this.barChartData = [{ data: this.ave, label: 'Ave State Cases', type: 'line' },
+    { data: this.data, label: 'Monthly Cases' }
+
+    ];
+    this.barChartLabels = ['FirstPlaceholder', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'LastPlaceholder'];
+    this.barChartOptions = {
+      responsive: true,
+      showLines: true,
+      maintainAspectRatio: true,
+      title: {
+        text: 'Local Monthly Cases',
+        display: true
+      },
+
+      // We use these empty structures as placeholders for dynamic theming.
+      scales: {
+        xAxes: [{
+          ticks: {
+
+            min: 'Nov',
+            max: 'Oct',
+
+          }
+        }], yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+
+    };
+  }
+  AllTimeChart() {
+    this.data = this.generatelineNumbers(10, 50);
+    this.ave = this.generateNumbers(50, 100);
+    this.barChartData = [
+      { data: this.ave, label: 'Ave State Cases', type: 'line' },
+      { data: this.data, label: 'All Local Time Cases' }
+    ];
+    this.barChartLabels = ['FirstPlaceholder', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', 'LastPlaceholder'];
+    this.barChartOptions = {
+      responsive: true,
+      showLines: true,
+      maintainAspectRatio: true,
+      title: {
+        text: 'Local All Time Cases',
+        display: true
+      },
+
+      // We use these empty structures as placeholders for dynamic theming.
+      scales: {
+        xAxes: [{
+          ticks: {
+
+            min: '11',
+            max: '0',
+
+          }
+        }], yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+
+    };
+  }
 }
+
